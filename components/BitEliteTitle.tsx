12b3c94dbeb72fd3ae1b637902694dd897922a8c
JithinKS <jithinks112@gmail.com>
2022-12-03 01:10:23 +0530

    header

---
+++ components/BitEliteTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Heading, Text } from '@chakra-ui/react'
+
+export default function BitEliteTitle() {
+    return (
+        <>
+            <Heading textAlign="center" size="3xl">BitElite</Heading>
+            <Text textAlign="center" color="gray.500" fontSize='2xl' w="4xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,15 @@ import {
 } from "@chakra-ui/react"
 import UploadFile from "../components/UploadFile"
 import Gradient from "../components/Gradient"
+import Header from "../components/Header"
 
 export default function Home() {
 
   return (
     <Container maxW="100vw" w="100vw" h="100vh">
       <Flex h="100%" direction="column" alignItems="center">
-
+        <Header />
         <Gradient />
-        <Heading textAlign="center" size="3xl">BitElite</Heading>
-        <Text textAlign="center" color="gray.500" fontSize='2xl' w="4xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
         <UploadFile />
         <Spacer />
       </Flex>
