eaf78462898afcd592f289bf7fdeb507d234c6a3
JithinKS <jithinks112@gmail.com>
2022-12-03 09:19:20 +0530

    file upload api

--- api/file.ts
+++ api/file.ts
@@ -1,8 +1,11 @@
 import axios from "../custom/axios";
+import config from "../config";
 
-export const sendFile = async (url: string, file: File) => {
+const { BASE_URL } = config;
+
+export const sendFile = async (file: File) => {
     const formData = new FormData();
-    formData.append("file", file);
-    const response = await axios.post(url, formData);
+    formData.append(`file`, file);
+    const response = await axios.post(`${BASE_URL}/file`, formData);
     return response.data;
 }
--- api/signature.ts
+++ api/signature.ts
@@ -3,7 +3,6 @@ import config from "../config";
 
 const { BASE_URL } = config;
 
-
 export const sendSignature = async (signature: string) => {
     const response = await axios.post(`${BASE_URL}/v1/user`, { signed_msg:signature });
     return response.data;
