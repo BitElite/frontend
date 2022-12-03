313e339ff048611b43b54ea83236c4216083f733
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-03 21:03:56 +0530

    add:Check Balance Btn

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -17,12 +17,13 @@ import {
 	Button,
 	Flex,
 	Divider,
-} from '@chakra-ui/react'
-import { CopyIcon } from "@chakra-ui/icons"
-import React from 'react'
-import dayjs from "dayjs"
-import { useAppSelector } from "../hooks/redux"
-import UploadFile from '../components/file/UploadFile';
+	Spacer,
+} from "@chakra-ui/react";
+import { CopyIcon } from "@chakra-ui/icons";
+import React from "react";
+import dayjs from "dayjs";
+import { useAppSelector } from "../hooks/redux";
+import UploadFile from "../components/file/UploadFile";
 
 const truncateString = (str: string, num: number) => {
 	if (str.length <= num) {
@@ -46,11 +47,16 @@ export default function Dashboard() {
 					</CardHeader>
 					<CardBody>
 						<Text>
-							Withdraw the amount from the smart contract to your wallet.
+							Withdraw the amount from the smart contract to your
+							wallet.
 						</Text>
 					</CardBody>
 					<CardFooter>
-						<Button>Withdraw</Button>
+						<Flex w={"100%"}>
+							<Button>Withdraw</Button>
+						    <Spacer />
+							<Button>Check Balance</Button>
+						</Flex>
 					</CardFooter>
 				</Card>
 				<Card width={"45%"}>
@@ -58,9 +64,7 @@ export default function Dashboard() {
 						<Heading size="md">Upload a file</Heading>
 					</CardHeader>
 					<CardBody>
-						<Text>
-							Upload any file to Filecoin network.
-						</Text>
+						<Text>Upload any file to Filecoin network.</Text>
 					</CardBody>
 					<CardFooter>
 						<UploadFile />
@@ -74,9 +78,9 @@ export default function Dashboard() {
 					</CardHeader>
 					<CardBody>
 						<Text>File Name</Text>
-						<Divider orientation='horizontal' my={"5"} />
+						<Divider orientation="horizontal" my={"5"} />
 						<Text>CID</Text>
-						<Divider orientation='horizontal' my={"5"} />
+						<Divider orientation="horizontal" my={"5"} />
 						<Text>Size</Text>
 					</CardBody>
 					<CardFooter
@@ -85,77 +89,82 @@ export default function Dashboard() {
 						<Button>Pay</Button>
 					</CardFooter>
 				</Card>
-			<TableContainer w="90%" my={"10"} overflow="hidden">
-				<Table
-					variant="unstyled"
-					bgColor="blackAlpha.400"
-					borderRadius="10px"
-				>
-					<Thead>
-						<Tr>
-							<Th>Name</Th>
-							<Th textAlign="center">CID</Th>
-							<Th>Size</Th>
-							<Th>Date</Th>
-						</Tr>
-					</Thead>
-					<Tbody>
-						{/* @ts-ignore */}
-						{files.map((file, index) => (
-							<Tr key={"row-" + index} _hover={{
-								backgroundColor: "blackAlpha.500"
-							}}>
-								<Td>
-									<Text>{file.name}</Text>
-								</Td>
-								<Td
-									style={{
-										display: "flex",
-										alignItems: "center",
-										justifyContent: "center",
+				<TableContainer w="90%" my={"10"} overflow="hidden">
+					<Table
+						variant="unstyled"
+						bgColor="blackAlpha.400"
+						borderRadius="10px"
+					>
+						<Thead>
+							<Tr>
+								<Th>Name</Th>
+								<Th textAlign="center">CID</Th>
+								<Th>Size</Th>
+								<Th>Date</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							{/* @ts-ignore */}
+							{files.map((file, index) => (
+								<Tr
+									key={"row-" + index}
+									_hover={{
+										backgroundColor: "blackAlpha.500",
 									}}
 								>
-									<Text fontFamily={"monospace"}>
-										{truncateString(file.cid, 7)}
-									</Text>
-									<IconButton
-										variant="outline"
-										colorScheme="teal"
-										aria-label="Copy CID"
-										size="sm"
-										icon={<CopyIcon />}
+									<Td>
+										<Text>{file.name}</Text>
+									</Td>
+									<Td
 										style={{
-											marginLeft: "10px",
+											display: "flex",
+											alignItems: "center",
+											justifyContent: "center",
 										}}
-										onClick={() => {
-											navigator.clipboard.writeText(
-												file.cid
-											);
-											toast({
-												title: "Copied CID",
-												description:
-													"The CID has been copied to your clipboard",
-												status: "success",
-												duration: 3000,
-												isClosable: true,
-											});
-										}}
-									/>
-								</Td>
-								<Td>
-									<Text>{file.size}</Text>
-								</Td>
-								<Td>
-									<Text>
-										{dayjs(file.uploadedAt).format("MMM D YYYY, h:mm a")}
-									</Text>
-								</Td>
-							</Tr>
-						))}
-					</Tbody>
-				</Table>
-			</TableContainer>
-		</Flex>
+									>
+										<Text fontFamily={"monospace"}>
+											{truncateString(file.cid, 7)}
+										</Text>
+										<IconButton
+											variant="outline"
+											colorScheme="teal"
+											aria-label="Copy CID"
+											size="sm"
+											icon={<CopyIcon />}
+											style={{
+												marginLeft: "10px",
+											}}
+											onClick={() => {
+												navigator.clipboard.writeText(
+													file.cid
+												);
+												toast({
+													title: "Copied CID",
+													description:
+														"The CID has been copied to your clipboard",
+													status: "success",
+													duration: 3000,
+													isClosable: true,
+												});
+											}}
+										/>
+									</Td>
+									<Td>
+										<Text>{file.size}</Text>
+									</Td>
+									<Td>
+										<Text>
+											{dayjs(file.uploadedAt).format(
+												"MMM D YYYY, h:mm a"
+											)}
+										</Text>
+									</Td>
+								</Tr>
+							))}
+						</Tbody>
+					</Table>
+				</TableContainer>
+			</Flex>
 		</>
 	);
 }
