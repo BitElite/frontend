856877604f2a10ff82da08d9f8c56098799d9ef4
unknown <sudhanplayz@gmail.com>
2022-12-03 17:54:28 +0530

    adjustment

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,15 @@
 import {
-    Table,
-    Thead,
-    Tbody,
-    Text,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-    Heading,
-    IconButton,
-    useToast,
+	Table,
+	Thead,
+	Tbody,
+	Text,
+	Tr,
+	Th,
+	Td,
+	TableContainer,
+	Heading,
+	IconButton,
+	useToast,
 	Card,
 	CardHeader,
 	CardBody,
@@ -57,7 +57,7 @@ export default function Dashboard() {
 					</CardHeader>
 					<CardBody>
 						<Text>
-							Upload any file to Filecoin network. 
+							Upload any file to Filecoin network.
 						</Text>
 					</CardBody>
 					<CardFooter>
@@ -65,75 +65,75 @@ export default function Dashboard() {
 					</CardFooter>
 				</Card>
 			</Flex>
-				<TableContainer w="90%" marginTop="10px">
-					<Table
-						variant="unstyled"
-						bgColor="blackAlpha.400"
-						borderRadius="10px"
-					>
-						<Thead>
-							<Tr>
-								<Th>Name</Th>
-								<Th textAlign="center">CID</Th>
-								<Th>Size</Th>
-								<Th>Date</Th>
-							</Tr>
-						</Thead>
-						<Tbody>
-							{files.map((file, index) => (
-								<Tr key={"row-" + index} _hover={{
-                                    backgroundColor: "blackAlpha.500"
-                                }}>
-									<Td>
-										<Text>{file.name}</Text>
-									</Td>
-									<Td
+			<TableContainer w="90%" marginTop="10px" overflow="hidden">
+				<Table
+					variant="unstyled"
+					bgColor="blackAlpha.400"
+					borderRadius="10px"
+				>
+					<Thead>
+						<Tr>
+							<Th>Name</Th>
+							<Th textAlign="center">CID</Th>
+							<Th>Size</Th>
+							<Th>Date</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{files.map((file, index) => (
+							<Tr key={"row-" + index} _hover={{
+								backgroundColor: "blackAlpha.500"
+							}}>
+								<Td>
+									<Text>{file.name}</Text>
+								</Td>
+								<Td
+									style={{
+										display: "flex",
+										alignItems: "center",
+										justifyContent: "center",
+									}}
+								>
+									<Text fontFamily={"monospace"}>
+										{truncateString(file.cid, 7)}
+									</Text>
+									<IconButton
+										variant="outline"
+										colorScheme="teal"
+										aria-label="Copy CID"
+										size="sm"
+										icon={<CopyIcon />}
 										style={{
-											display: "flex",
-											alignItems: "center",
-											justifyContent: "center",
+											marginLeft: "10px",
 										}}
-									>
-										<Text fontFamily={"monospace"}>
-											{truncateString(file.cid, 7)}
-										</Text>
-										<IconButton
-											variant="outline"
-											colorScheme="teal"
-											aria-label="Copy CID"
-											size="sm"
-											icon={<CopyIcon />}
-											style={{
-												marginLeft: "10px",
-											}}
-											onClick={() => {
-												navigator.clipboard.writeText(
-													file.cid
-												);
-												toast({
-													title: "Copied CID",
-													description:
-														"The CID has been copied to your clipboard",
-													status: "success",
-													duration: 3000,
-													isClosable: true,
-												});
-											}}
-										/>
-									</Td>
-									<Td>
-										<Text>{file.size}</Text>
-									</Td>
-									<Td>
-										<Text>
-											{dayjs(file.uploadedAt).format("MMM D YYYY, h:mm a")}
-										</Text>
-									</Td>
-								</Tr>
-							))}
-						</Tbody>
-					</Table>
-				</TableContainer>
+										onClick={() => {
+											navigator.clipboard.writeText(
+												file.cid
+											);
+											toast({
+												title: "Copied CID",
+												description:
+													"The CID has been copied to your clipboard",
+												status: "success",
+												duration: 3000,
+												isClosable: true,
+											});
+										}}
+									/>
+								</Td>
+								<Td>
+									<Text>{file.size}</Text>
+								</Td>
+								<Td>
+									<Text>
+										{dayjs(file.uploadedAt).format("MMM D YYYY, h:mm a")}
+									</Text>
+								</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</TableContainer>
 		</>
 	);
 }
