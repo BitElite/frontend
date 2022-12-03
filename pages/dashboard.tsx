b43981b0d4985d385c2fd50570bbf40108296056
unknown <sudhanplayz@gmail.com>
2022-12-03 12:09:03 +0530

    feat: List all files uploaded

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,105 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Text,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+    Heading,
+    IconButton,
+    useToast
+} from '@chakra-ui/react'
+import { CopyIcon } from "@chakra-ui/icons"
 import React from 'react'
 
+const files = [
+    {
+        name: "No name",
+        size: "0.80 MB",
+        cid: "bafybeifued2wl2nkkb4xddpmiusgv22rfsgxq23vg5dwyjvtczovpw6j4m",
+        uploadedAt: new Date()
+    },
+    {
+        name: "No name",
+        size: "0.80 MB",
+        cid: "bafybeifued2wl2nkkb4xddpmiusgv22rfsgxq23vg5dwyjvtczovpw6j4m",
+        uploadedAt: new Date()
+    },
+    {
+        name: "No name",
+        size: "0.80 MB",
+        cid: "bafybeifued2wl2nkkb4xddpmiusgv22rfsgxq23vg5dwyjvtczovpw6j4m",
+        uploadedAt: new Date()
+    },
+    {
+        name: "No name",
+        size: "0.80 MB",
+        cid: "bafybeifued2wl2nkkb4xddpmiusgv22rfsgxq23vg5dwyjvtczovpw6j4m",
+        uploadedAt: new Date()
+    }
+]
+
+const truncateString = (str: string, num: number) => {
+    if (str.length <= num) {
+        return str
+    }
+    return str.slice(0, num) + '...' + str.slice(str.length - num, str.length)
+}
+
 export default function Dashboard() {
+    const toast = useToast()
+
     return (<>
-        
+        <Heading>Dashboard</Heading>
+        <TableContainer w="90%" marginTop="10px">
+            <Table variant='unstyled' bgColor="blackAlpha.400" borderRadius="10px">
+                <Thead>
+                    <Tr>
+                        <Th>Name</Th>
+                        <Th textAlign="center">CID</Th>
+                        <Th>Size</Th>
+                        <Th>Date</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {files.map((file, index) => (
+                        <Tr key={"row-" + index}>
+                            <Td><Text>{file.name}</Text></Td>
+                            <Td style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}>
+                                <Text fontFamily={"monospace"}>{truncateString(file.cid, 10)}</Text>
+                                <IconButton
+                                    variant='outline'
+                                    colorScheme='teal'
+                                    aria-label='Copy CID'
+                                    size="sm"
+                                    icon={<CopyIcon />}
+                                    style={{
+                                        marginLeft: "10px"
+                                    }}
+                                    onClick={() => {
+                                        navigator.clipboard.writeText(file.cid)
+                                        toast({
+                                            title: "Copied CID",
+                                            description: "The CID has been copied to your clipboard",
+                                            status: "success",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        })
+                                    }}
+                                />
+                            </Td>
+                            <Td><Text>{file.size}</Text></Td>
+                            <Td><Text>{file.uploadedAt.toLocaleDateString()}</Text></Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </Table>
+        </TableContainer>
     </>)
 }
