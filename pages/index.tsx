aeea8c8e8df4e72e84e18eabfd5fd7d035e127ce
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-04 08:59:02 +0530

    change background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,20 @@
-import {
-  Container,
-  Flex,
-  Spacer,
-  Tooltip
-} from "@chakra-ui/react"
-import Gradient from "../components/home/Gradient"
-import BitEliteTitle from "../components/home/BitEliteTitle"
-import Connect from "../components/home/Connect"
-import Head from "next/head"
+import { Container, Flex, Spacer, Tooltip } from "@chakra-ui/react";
+import Gradient from "../components/home/Gradient";
+import BitEliteTitle from "../components/home/BitEliteTitle";
+import Connect from "../components/home/Connect";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>BitElite</title>
-      </Head>
-      <Gradient />
-      <BitEliteTitle />
-      <Connect />
-      <Spacer />
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>BitElite</title>
+			</Head>
+			<Gradient />
+			<div style={{position:"absolute",bottom:"100px",right:"700px"}}>
+				<Connect />
+			</div>
+			<Spacer />
+		</>
+	);
 }
