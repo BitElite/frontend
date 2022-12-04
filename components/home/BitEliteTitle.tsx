87d58840bbeb81cd1b4b33c5cb691fcd64432319
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-04 08:08:35 +0530

    changed title

--- components/home/BitEliteTitle.tsx
+++ components/home/BitEliteTitle.tsx
@@ -4,7 +4,7 @@ import { Heading, Text } from '@chakra-ui/react'
 export default function BitEliteTitle() {
     return (
         <>
-            <Heading textAlign="center" size="3xl">BitElite</Heading>
+            <Heading textAlign="center" size="3xl">Deduplication layer</Heading>
             <Text marginTop="1rem" textAlign="center" color="gray.500" fontSize='2xl' w="4xl">
                 Upload files to filecoin, but pay less.
             </Text>
